bcf1e623e19bae1d6d2963ffca7718f954457318
matt1510 <32223860+matt1510@users.noreply.github.com>
2023-04-02 22:08:25 +0100

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const text = string.split("");
+    reversedArray = text.reverse();
+    return reversedArray.join("");
 
 };
 
