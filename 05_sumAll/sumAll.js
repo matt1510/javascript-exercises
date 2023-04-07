700adc2a8f658b17ed3ca12343e0c9fccca13eed
matt1510 <32223860+matt1510@users.noreply.github.com>
2023-04-07 23:03:31 +0100

    Completed exercise 05_sumAll, all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let smaller = 0;
+    let larger = 0;
+    let sum = 0;
+    function counter(smaller, larger) {
+        for (let i = smaller; i <= larger; i++){
+            sum += i;
+        }
+    };
 
-};
+    if (start < 0 || end < 0 || typeof start != 'number' || typeof end != 'number'){
+        return 'ERROR';
+    } else if (end < start){
+        smaller = end;
+        larger = start;
+        counter(smaller, larger);
+    } else {
+        smaller = start;
+        larger = end;
+        counter(smaller, larger);
+        }
+    return sum;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
