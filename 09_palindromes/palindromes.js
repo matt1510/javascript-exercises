bde2a0c3f07606714f8deff46a766070bb6cf59a
matt1510 <32223860+matt1510@users.noreply.github.com>
2023-05-08 18:10:22 +0100

    palindrome checker complete with passing tests. Convert to lower case and remove special characters then compare to the reverse of itself.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    nothing = text.toLowerCase().replace(/[^a-z]/g, '');
+    reversed = nothing.split('').reverse().join('');
+    if (nothing === reversed) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
+//palindromes('Racecar!')
 
 // Do not edit below this line
 module.exports = palindromes;
