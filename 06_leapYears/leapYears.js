bc3acf74bacad834f2ae465cb22097417bec3103
matt1510 <32223860+matt1510@users.noreply.github.com>
2023-05-08 17:55:46 +0100

    working calculator functions. all test passing

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,6 @@
 const leapYears = function(year) {
-    if (year % 100 === 0 && year % 400 === 0 && year % 4 === 0){
-        return true;
-    } else if (year % 4 === 0 && year % 100 != 0){
-        return true;
-    } else {
-        return false;
-    }
-
-};
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    };
 
 // Do not edit below this line
 module.exports = leapYears;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	for (i = 0; i< array.length; i++) {
+    return array.reduce((total, n) => total + n);
+  }
+  return 0
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  for (i = 0; i< array.length; i++) {
+    return array.reduce((total, n) => total * n);
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1)
+  return 1;
+  for (var i = a - 1; i >= 1; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
